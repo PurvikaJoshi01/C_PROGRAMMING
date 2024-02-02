30392eaaa6683bb909492cb26c7b01971e943fec
PurvikaJoshi01 <113506230+PurvikaJoshi01@users.noreply.github.com>
2024-02-02 13:27:03 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+// square and cube of a number
+#include<stdio.h>
+int main(){
+    int x;
+    printf("enter the value of x");
+    scanf("%d",&x);
+    printf("square of the number x is %d:", x*x);
+    printf("cube of the number x is %d :",x*x*x);
+}
+
